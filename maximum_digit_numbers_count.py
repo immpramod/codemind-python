5d866ebeecc46fdacfd19126da8dc05f0a3be433
pramod kumar <95994718+immpramod@users.noreply.github.com>
2023-08-14 14:37:55 +0530

    def lenj(s):     if s<0:         s-1*s     return len(str(s)) n=int(input()) l=list(map(int,input().split())) k=[] for i in l:     k.append(lenj(i)) p=max(k) for i in l:     if lenj(i)==p:         print(i,end=' ')

---
+++ maximum_digit_numbers_count.py
@@ -0,0 +1,13 @@
+def lenj(s):
+    if s<0:
+        s-1*s
+    return len(str(s))
+n=int(input())
+l=list(map(int,input().split()))
+k=[]
+for i in l:
+    k.append(lenj(i))
+p=max(k)
+for i in l:
+    if lenj(i)==p:
+        print(i,end=' ')
