e93098a3dd0eaa2cde29c31b51994d9a22e2f3b5
pramod kumar <95994718+immpramod@users.noreply.github.com>
2023-08-23 17:58:30 +0530

    n=int(input()) for i in range(n):     s=input()     c=0     for i in s:         if(i.isdigit()):             c+=1     if (c==len(s)):         print('True')     else:         print('False')

---
+++ String_decimal_.py
@@ -0,0 +1,11 @@
+n=int(input())
+for i in range(n):
+    s=input()
+    c=0
+    for i in s:
+        if(i.isdigit()):
+            c+=1
+    if (c==len(s)):
+        print('True')
+    else:
+        print('False')
